c62a8d94619e30e60e504ce055c9a6846acdeb20
leprecha1 <mrleprecha1purplesentinel@gmail.com>
2023-10-02 14:46:02 -0400

    Modifying parameters

--- C/caesar-cypher/encryptCaesar.py
+++ C/caesar-cypher/encryptCaesar.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
+import sys
 
-story = "Teste"
+story = sys.argv[1]
 shift = 12
 ceasar = lambda s,n:''.join([(lambda c,u:c.upper() if u else c)(("abcdefghijklmnopqrstuvwxyz"*2)[ord(c.lower())-97+n%26],c.isupper())if c.isalpha() else c for c in s])
 print(ceasar(story, shift))
