32f6aa0d7d4981bb0633359b14f5e10a03e5f178
leprecha1 <mrleprecha1purplesentinel@gmail.com>
2023-10-02 14:48:27 -0400

    Modifying shift parameters

--- C/caesar-cypher/encryptCaesar.py
+++ C/caesar-cypher/encryptCaesar.py
@@ -2,6 +2,6 @@
 import sys
 
 story = sys.argv[1]
-shift = 12
+shift = sys.argv[2]
 ceasar = lambda s,n:''.join([(lambda c,u:c.upper() if u else c)(("abcdefghijklmnopqrstuvwxyz"*2)[ord(c.lower())-97+n%26],c.isupper())if c.isalpha() else c for c in s])
 print(ceasar(story, shift))
