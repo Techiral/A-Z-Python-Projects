5ebd1b096d6a7059bd7ffb73a87bd995c758a40d
Sarthak <bhandaresarthak074@gmail.com>
2023-10-10 22:00:13 +0530

    I have skillfully added choice of categories in jokes and user can enter any category accordingly

--- J/joke_generator.py
+++ J/joke_generator.py
@@ -2,7 +2,11 @@ import pyjokes
   
 # using get_joke() to generate a single joke
 #language is english
-#category is neutral
-My_joke = pyjokes.get_joke(language="en", category="neutral")
+print("Categories:- ")
+print("neutral")
+print("twister")
+print("all")
+s=str(input("Type your categories:- "))
+My_joke = pyjokes.get_joke(language="en", category=s)
   
 print(My_joke)
