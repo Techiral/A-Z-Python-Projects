334d29481b542cbc2dfd80e901f2c6c8432a57a2
Prashidika Tiwari <prashidika2061@gmail.com>
2023-10-14 10:57:13 +0545

    Simple Quiz Game

---
+++ Q/quiz_game/settings.py
@@ -0,0 +1,14 @@
+class Settings:
+    def __init__(self) -> None:
+        self.questions =[
+            {'question':'What is the capital of France?','A':'London','B':'Paris','C':'Berlin','D':'Madrid','correct':'B'},
+            {'question':'Which planet is known as Red planet?','A':'Venus','B':'Jupyter','C':'Mars','D':'Moon','correct':'C'},
+            {'question':'Who wrote the play Romeo & Juliet?','A':'Charles Dickens','B':'William Shakespeare','C':'Jane Austin','D':'Mark Twain','correct':'B'},
+            {'question':'What is the chemical symbol for Gold?','A':'Au','B':'Go','C':'Ag','D':'Ge','correct':'A'},
+            {'question':'What is the largest mammal in the world?','A':'Giraffe','B':'Whale','C':'Elephant','D':'Lion','correct':'C'},
+            {'question':'What is the primary gas that makes up earth atmosphere?','A':'Carbondioxide','B':'Nitrogen','C':'Oxygen','D':'Hydrogen','correct':'B'},
+            {'question':'Which country is famous for its pyramids?','A':'Egypt','B':'Greece','C':'Mexico','D':'Italy','correct':'A'},
+            {'question':'In which year did Christopher Columbus reach the Americas?','A':'1492','B':'1776','C':'1812','D':'1607','correct':'A'},
+            {'question':'Who is the author of "To Kill a Mockingbird"?','A':'George Orwell','B':'J.K Rowling','C':'Harper Lee','D':'Ernest Hemingway','correct':'C'},
+            {'question':'Which is the largest planet in the solar system?','A':'Mars','B':'Earth','C':'Saturn','D':'Jupiter','correct':'D'}
+        ]
