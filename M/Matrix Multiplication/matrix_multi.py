12332ef804a09eb328439a1e55b0d04d113ac3d3
pjha8040 <pjha8040@gmail.com>
2023-10-22 11:41:47 +0530

    Changed static input to user input

--- M/Matrix Multiplication/matrix_multi.py
+++ M/Matrix Multiplication/matrix_multi.py
@@ -1,11 +1,23 @@
+import numpy as np
+ 
+rows = 3
+col = 3
 
-X = [[8,7,3],
-    [4 ,6,6],
-    [5,10,9]]
-Y = [[8,8,1,2],
-    [6,10,3,5],
-    [8,5,9,1]]
+X = np.zeros((rows,col), dtype=int)
+Y = np.zeros((rows,col), dtype=int)
+
+def inpMatrix(matrix,rows,col):
+    for i in range (rows):
+        for j in range(col):
+            matrix[i,j] = int(input(f"Enter element for row{i+1}, column{j+1}: "))
+    print(f"Element for matrix: \n {matrix}")
+
+inpMatrix(X,rows,col)
+inpMatrix(Y,rows,col)
+
+print("\n")
+
 result = [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*Y)] for X_row in X]
-
+print("The result is : \n")
 for r in result:
    print(r)
